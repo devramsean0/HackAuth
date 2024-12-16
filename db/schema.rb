3ac957ccf7ffd2d83887ca201f73200818887f7f
Sean Outram <outramsean0@gmail.com>
2024-12-16 10:18:47 +0000

    Initial authentication setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_15_194646) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_095132) do
   create_table "applications", force: :cascade do |t|
     t.string "name"
     t.string "client_id"
@@ -30,5 +30,20 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_15_194646) do
     t.string "slack_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
